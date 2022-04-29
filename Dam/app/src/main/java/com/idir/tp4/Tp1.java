77823aa7ffad913bd02f8657270ad6c1996c544b
IDIRYACINE <idiryacinesp@gmail.com>
2022-04-29 14:22:15 +0100

    added toast message

--- Dam/app/src/main/java/com/idir/tp4/Tp1.java
+++ Dam/app/src/main/java/com/idir/tp4/Tp1.java
@@ -4,14 +4,31 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Tp1 extends Activity {
 
+    private EditText nameField,surnameField,addressField;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         View root = View.inflate(this,R.layout.activity_tp1, null);
+
+        nameField = root.findViewById(R.id.name_field);
+        surnameField = root.findViewById(R.id.surname_field);
+        addressField = root.findViewById(R.id.address_field);
+
+        Button confirmButton = root.findViewById(R.id.confirm_button);
+        confirmButton.setOnClickListener(view -> {
+            String name = nameField.getText().toString();
+            String surname = surnameField.getText().toString();
+            Toast.makeText(getApplicationContext(),name + " " + surname,Toast.LENGTH_SHORT).show();
+
+        });
+
         setContentView(root);
 
     }
