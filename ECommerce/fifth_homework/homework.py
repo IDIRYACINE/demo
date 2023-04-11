943f35fc5861ccb1345aa10d7e094654b4acf5b4
IDIRYACINE <idiryacinesp@gmail.com>
2023-04-11 15:33:25 +0000

    update

--- ECommerce/fifth_homework/homework.py
+++ ECommerce/fifth_homework/homework.py
@@ -2,15 +2,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load the dataset into a Pandas DataFrame
-df = pd.read_csv('E-commerce Dataset.csv')
+df = pd.read_csv('EcommerceDataset.csv')
 
 # Check the dataset for missing values and handle them appropriately
 print(df.isnull().sum())  # Check for missing values in each column
-df.dropna(inplace=True)   # Drop rows with missing values
+df.dropna()   # Drop rows with missing values
 
 # Check the dataset for duplicates and handle them appropriately
 print(df.duplicated().sum())  # Check for duplicates
-df.drop_duplicates(inplace=True)  # Drop duplicates
+df.drop_duplicates()  # Drop duplicates
 
 # Convert the Order_Date column to a datetime data type
 df['Order_Date'] = pd.to_datetime(df['Order_Date'])
@@ -27,32 +27,34 @@ sales_profit = df.groupby(['Product_Category', 'Product'])['Sales', 'Profit'].su
 print(sales_profit)
 
 # Visualize the total sales and profit for each product category and product using bar charts
-sales_profit.plot(kind='bar', figsize=(12, 6))
+sales_profit.plot(kind='bar', edgecolor="black",figsize=(12, 6))
 plt.title('Total Sales and Profit by Product Category and Product')
 plt.xlabel('Product Category, Product')
 plt.ylabel('Total Sales and Profit')
 plt.show()
 
 # Analyze and visualize the distribution of the shipping cost and profit
-df[['Shipping_Cost', 'Profit']].hist(figsize=(10, 5))
+df[['Shipping_Cost', 'Profit']].hist(edgecolor="black",figsize=(10, 5))
 plt.suptitle('Distribution of Shipping Cost and Profit')
 plt.show()
 
+
 # Identify the most popular payment methods used by customers
-payment_methods = df['Payment_Method'].value_counts()
+payment_methods = df['Payment_method'].value_counts()
 print(payment_methods)
 
 # Analyze and visualize the distribution of the aging of the orders
+df['Order_Date'] = pd.to_datetime(df['Order_Date'])  # convert to Timestamp object
 df['Order_Age'] = (pd.to_datetime('today') - df['Order_Date']).dt.days
-df['Order_Age'].hist()
+df['Order_Age'].hist(edgecolor="black")
 plt.title('Distribution of Order Age')
 plt.xlabel('Order Age (days)')
 plt.ylabel('Count')
 plt.show()
 
 # Explore the relationship between the device type and the order priority
-device_order_priority = df.groupby(['Device_Type', 'Order_Priority'])['Order_ID'].count()
-device_order_priority.plot(kind='bar', figsize=(10, 5))
+device_order_priority = df.groupby(['Device_Type', 'Order_Priority'])["Time"].count()
+device_order_priority.plot(kind='bar', edgecolor="black",figsize=(10, 5))
 plt.title('Device Type vs. Order Priority')
 plt.xlabel('Device Type, Order Priority')
 plt.ylabel('Count')
@@ -60,7 +62,7 @@ plt.show()
 
 # Explore the relationship between the product category, product, and sales
 category_product_sales = df.groupby(['Product_Category', 'Product'])['Sales'].sum()
-category_product_sales.plot(kind='bar', figsize=(10, 5))
+category_product_sales.plot(kind='bar', edgecolor="black",figsize=(10, 5))
 plt.title('Product Category, Product vs. Sales')
 plt.xlabel('Product Category, Product')
 plt.ylabel('Sales')
--- ECommerce/fourth_homework/homework.py
+++ ECommerce/fourth_homework/homework.py
@@ -1,50 +1,104 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Load and read the data
 df = pd.read_csv('e-commerce.csv')
 
 # Check and display the information summary of the dataset
 print(df.info())
+print("#--------------------------")
 
 # Print the number of missing data for each variable
 print(df.isnull().sum())
+print("#--------------------------")
 
 # Print the number of duplicates for each variable
 print(df.duplicated().sum())
+print("#--------------------------")
 
 # Handle duplicates by dropping them
-df.drop_duplicates(inplace=True)
+df.drop_duplicates()
+print("#--------------------------")
 
 
 # Remove the missing data rows for <Product_Category_2>
-df.dropna(subset=['Product_Category_2'], inplace=True)
+df.dropna(subset=['Product_Category_2'], )
+print("#--------------------------")
 
 # Fill the missing data using the appropriate method for <Product_Category_3>
-df['Product_Category_3'].fillna(df['Product_Category_3'].mode()[0], inplace=True)
+df['Product_Category_3'].fillna(df['Product_Category_3'].mode()[0], )
+print("#--------------------------")
 
 # Encode categorical variables using one-hot encoding
 df_encoded = pd.get_dummies(df, columns=['Gender', 'City_Category', 'Age', 'Stay_In_Current_City_Years'])
 
 # Show a random sample of 15 rows from the dataset
 print(df_encoded.sample(n=15))
+print("#--------------------------")
 
 # Show the first 7 lines of the dataset
 print(df_encoded.head(7))
+print("#--------------------------")
 
 # Show the last 3 lines of the dataset
 print(df_encoded.tail(3))
+print("#--------------------------")
 
-plt.hist(df_encoded['Purchase'], bins=20)
-plt.xlabel('Purchase')
-plt.ylabel('Frequency')
-plt.title('Distribution of Purchase')
+# Create a 3D histogram plot
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+purchase = df['Purchase']
+hist, xedges, yedges = np.histogram2d(purchase,np.ones(len(purchase)), bins=20)
+
+# Construct arrays for the anchor positions of the bars
+xpos, ypos = np.meshgrid(xedges[:-1] + 0.25, yedges[:-1] + 0.25, indexing="ij")
+xpos = xpos.ravel()
+ypos = ypos.ravel()
+zpos = 3
+
+# Construct arrays with the dimensions for the bars
+dx = dy = 0.5 * np.ones_like(zpos)
+dz = hist.ravel()
+
+# Create the 3D bar plot
+ax.bar3d(xpos, ypos, zpos, dx, dy, dz, color='b', zsort='average')
+
+# Set the axis labels and title
+ax.set_xlabel('Purchase')
+ax.set_ylabel('Z')
+ax.set_zlabel('Frequency')
+ax.set_title('Distribution of Purchase')
+
+# Show the plot
 plt.show()
+
+print("#--------------------------")
 
 # Calculate summary statistics for each variable
 summary_stats = df_encoded.describe()
 print(summary_stats)
 
+print("#--------------------------")
+
 # Compare the distributions of the variables for different categories using groupby()
-grouped_data = df_encoded.groupby(['Age', 'Gender'])['Purchase'].mean().reset_index()
+#'Age_0-17', 'Age_18-25','Age_26-35', 'Age_36-45', 'Age_46-50', 'Age_51-55', 'Age_55+'
+grouped_data = df_encoded.groupby('Age_0-17')['Purchase'].describe()
 print(grouped_data)
+
+grouped_data = df_encoded.groupby('Age_18-25')['Purchase'].describe()
+print(grouped_data)
+
+grouped_data = df_encoded.groupby('Age_26-35')['Purchase'].describe()
+print(grouped_data)
+
+grouped_data = df_encoded.groupby('Age_46-50')['Purchase'].describe()
+print(grouped_data)
+
+grouped_data = df_encoded.groupby('Age_51-55')['Purchase'].describe()
+print(grouped_data)
+
+grouped_data = df_encoded.groupby('Age_55+')['Purchase'].describe()
+print(grouped_data)
+print("#--------------------------")
